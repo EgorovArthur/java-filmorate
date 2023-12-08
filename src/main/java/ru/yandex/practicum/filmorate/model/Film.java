c1cfc304e66fb44b06497faf2755975e2357288e
EgorovArtur <arthurmsk12154@yandex.ru>
2023-12-08 19:07:32 +0300

    Выполенение ФЗ-9

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -2,8 +2,6 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
